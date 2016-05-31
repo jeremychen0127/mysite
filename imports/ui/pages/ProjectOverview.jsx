0e1376b81bad12fe9a2a84fc5688648b95fa825e
JeremyChen <jeremychen0127@gmail.com>
2016-05-30 23:30:15 -0400

    Added ProjectOverview page

---
+++ imports/ui/pages/ProjectOverview.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import ProjectGrids from '../components/projects/ProjectGrids';
+import UnderConstruction from '../components/placeholder/UnderContruction';
+
+export default class ProjectOverview extends Component {
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  }
+  render() {
+    return (
+      <div className="center">
+        <br />
+        <UnderConstruction />
+      </div>
+    );
+  }
+}
+
+ProjectOverview.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired,
+};
