370b15c860b69bd6ab08f3d491b482f041cc4cd8
JeremyChen <jeremychen0127@gmail.com>
2016-06-25 13:18:47 -0400

    Changed the style of UnderContruction

--- imports/ui/components/placeholder/UnderContruction.jsx
+++ imports/ui/components/placeholder/UnderContruction.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 
+const style = {
+  borderRadius: '20px',
+  color: '#455a64',
+};
+
 export default class UnderContruction extends Component {
   render() {
     return (
       <div className="row">
         <div className="col offset-m2 m8 s12">
           <div className="center">
-            <Paper zDepth={4}>
+            <Paper zDepth={4} style={style}>
               <br />
               <br />
-              <h2>Coming Soon!</h2>
-              <h2>Stay Tuned!</h2>
+              <h4>Coming Soon!</h4>
+              <h4>Stay Tuned!</h4>
               <br />
               <br />
             </Paper>
+            <br />
+            <br />
           </div>
         </div>
       </div>
