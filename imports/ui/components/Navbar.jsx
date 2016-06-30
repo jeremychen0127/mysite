2bb64813294e6ab9d3b1fa9fe4ed1fe35198a05d
JeremyChen <jeremychen0127@gmail.com>
2016-06-30 00:45:23 -0400

    Added left nav

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,29 +1,85 @@
 import React, { Component } from 'react';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { createContainer } from 'meteor/react-meteor-data';
+import { FlowRouter } from 'meteor/kadira:flow-router'
 
-export default class TopNavbar extends Component {
+import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import MenuItem from 'material-ui/MenuItem';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      leftNavOpen: false,
+    };
+
+    this.handleLeftNavToggle = () =>this.setState({leftNavOpen: !this.state.leftNavOpen});
+  }
+
   getChildContext() {
     return {muiTheme: getMuiTheme(baseTheme)};
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({leftNavOpen: false});
+  }
+
+  renderLeftNav() {
+    return (
+      <Drawer open={this.state.leftNavOpen} docked={false}
+              onRequestChange={(open, reason) => { this.setState({leftNavOpen:open}) }}
+              width={200} style={{zIndex:9999}}>
+        <MenuItem linkButton={true} href="/profile" primaryText="Profile"
+                  leftIcon={<FontIcon className="material-icons">account_box</FontIcon>}/>
+        <MenuItem linkButton={true} href="/experience" primaryText="Experience"
+                  leftIcon={<FontIcon className="material-icons">build</FontIcon>}/>
+        <Divider />
+        <MenuItem linkButton={true} href="/resume" primaryText="Resume"
+                  leftIcon={<FontIcon className="material-icons">assignment</FontIcon>}/>
+      </Drawer>
+    );
+  }
+
   render() {
-    let appBarStyle = {
-      backgroundColor: '#78909c'
-    };
     return (
       <div className="navbar">
         <ul>
-          <li><a id="logo" href="/">Jeremy Chen</a></li>
-          <li><a href="/profile">Profile</a></li>
-          <li><a href="/experience">Experience</a></li>
-          <li className="right"><a href="/resume">Resume</a></li>
+          <div className="hide-on-large-only">
+            <li>
+              <IconButton iconStyle={{marginTop: '5px'}} onTouchTap={this.handleLeftNavToggle}>
+                <FontIcon color="white" className="material-icons">menu</FontIcon>
+              </IconButton>
+            </li>
+          </div>
+
+          <div>
+            <li><a id="logo" href="/">Jeremy Chen</a></li>
+          </div>
+
+          <div className="hide-on-med-and-down">
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="/experience">Experience</a></li>
+            <li className="right"><a href="/resume">Resume</a></li>
+          </div>
         </ul>
+        {this.renderLeftNav()}
       </div>
     );
   }
 }
 
-TopNavbar.childContextTypes = {
+Navbar.childContextTypes = {
   muiTheme: React.PropTypes.object.isRequired
 };
+
+export default createContainer(() => {
+  return { currentRoute: FlowRouter.getRouteName() };
+}, Navbar);
