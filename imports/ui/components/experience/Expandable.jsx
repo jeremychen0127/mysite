9f16e277afd8165b7a2ebff84bc0d5a82266bdf0
JeremyChen <jeremychen0127@gmail.com>
2016-07-01 00:32:41 -0400

    Made expandable max height responsive

--- imports/ui/components/experience/Expandable.jsx
+++ imports/ui/components/experience/Expandable.jsx
@@ -6,16 +6,36 @@ export default class Expandable extends Component {
 
     this.state = {
       isExpand: false,
+      windowWidth: window.innerWidth,
+      maxHeight: '500px',
     };
 
     this.toggleExpand = () => this.setState({isExpand: !this.state.isExpand});
+    this.handleResize = (event) => {
+      if (window.innerWidth >= 1350) {
+        this.setState({maxHeight: '200px'});
+      } else if (window.innerWidth >= 1000) {
+        this.setState({maxHeight: '300px'});
+      } else {
+        this.setState({maxHeight: '500px'});
+      }
+      this.setState({windowWidth: window.innerWidth});
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
   }
 
   render() {
     return (
         <div className="experienceExpandable">
           <img src={this.props.imgSrc} width="400" height="120" style={{cursor: 'pointer'}} onClick={this.toggleExpand}/>
-          <div className="expDescription" style={{maxHeight: this.state.isExpand ? '500px' : '0px'}}>
+          <div className="expDescription" style={{maxHeight: this.state.isExpand ? this.state.maxHeight : '0px'}}>
             <div className="row">
               <div className="col m8 offset-m2 s10 offset-s1">
                 <p>{this.props.description}</p>
