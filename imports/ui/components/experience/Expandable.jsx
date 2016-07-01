497b2b303ac866d1bdfe22d1aee1ba5cfea7440d
JeremyChen <jeremychen0127@gmail.com>
2016-07-01 00:08:15 -0400

    Used onClick instead of hovering to expand

--- imports/ui/components/experience/Expandable.jsx
+++ imports/ui/components/experience/Expandable.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Expandable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isExpand: false,
+    };
+
+    this.toggleExpand = () => this.setState({isExpand: !this.state.isExpand});
+  }
+
   render() {
     return (
         <div className="experienceExpandable">
-          <img src={this.props.imgSrc} width="400" height="120"/>
-          <div className="expDescription">
+          <img src={this.props.imgSrc} width="400" height="120" style={{cursor: 'pointer'}} onClick={this.toggleExpand}/>
+          <div className="expDescription" style={{maxHeight: this.state.isExpand ? '500px' : '0px'}}>
             <div className="row">
               <div className="col m8 offset-m2 s10 offset-s1">
                 <p>{this.props.description}</p>
