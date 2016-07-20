b94d64527170766946dfa86d5d674118339a5cd1
JeremyChen <jeremychen0127@gmail.com>
2016-07-19 22:42:01 -0400

    Ability to submit contact me form

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -13,6 +13,26 @@ const styles = {
 };
 
 export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    };
+
+    this.handleNameChange = (event) => this.setState({name: event.currentTarget.value});
+    this.handleEmailChange = (event) => this.setState({email: event.currentTarget.value});
+    this.handleMessageChange = (event) => this.setState({message: event.currentTarget.value});
+
+    this.handleSubmit = (event) => {
+      event.preventDefault();
+
+      this.setState({name: '', email: '', message: ''});
+    }
+  }
+
   getChildContext() {
     return {muiTheme: getMuiTheme(baseTheme)};
   }
@@ -28,21 +48,24 @@ export default class Footer extends Component {
         <div className="row">
           <div className="col offset-s1 s10 offset-m2 m4 offset-l3 l3" style={styles.inputDiv}>
             <label htmlFor="name" className="input-label">Your Name</label>
-            <input id="name" type="text" placeholder="e.g. Larry Page" className="validate" style={styles.inputField}/>
+            <input id="name" type="text" placeholder="e.g. Larry Page" className="validate"
+                   value={this.state.name} onChange={this.handleNameChange} style={styles.inputField}/>
           </div>
           <div className="col offset-s1 s10 m4 l3" style={styles.inputDiv}>
             <label htmlFor="email" className="input-label">Your Email</label>
-            <input id="email" type="email" placeholder="e.g. LarryPage@gmail.com" className="validate" style={styles.inputField}/>
+            <input id="email" type="email" placeholder="e.g. LarryPage@gmail.com" className="validate"
+                   value={this.state.email} onChange={this.handleEmailChange} style={styles.inputField}/>
           </div>
         </div>
         <div className="row" style={{marginBottom: '0px'}}>
           <div className="col offset-s1 s10 offset-m2 m6 offset-l3 l4" style={styles.inputDiv}>
             <label htmlFor="message" className="input-label">Message</label>
-            <textarea id="message" className="materialize-textarea" style={styles.inputField}/>
+            <textarea id="message" className="materialize-textarea" value={this.state.message}
+                      onChange={this.handleMessageChange} style={styles.inputField}/>
           </div>
           <div className="col s12 m2 l2">
             <div className="center" style={{paddingTop: '40px'}}>
-              <RaisedButton label="Send" />
+              <RaisedButton label="Send" onClick={this.handleSubmit}/>
             </div>
           </div>
         </div>
