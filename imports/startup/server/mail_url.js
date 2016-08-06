7fa4245965087e11951e8388ab01cc3a8ff27a2a
JeremyChen <jeremychen0127@gmail.com>
2016-08-05 23:40:39 -0400

    Added mailgun MAIL_URL env var

---
+++ imports/startup/server/mail_url.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+  process.env.MAIL_URL = 'smtp://postmaster%40sandboxd2fd2c8ce98540eab33f0bbcc90ee6f7.mailgun.org:8cad4fc5ed4472cbfbb210b1cfcc6ff9@smtp.mailgun.org:587';
+});
