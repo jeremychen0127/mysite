a7aa0099908216210c48db4637e5a5f2f96cfbcc
JeremyChen <jeremychen0127@gmail.com>
2016-08-13 22:11:47 -0400

    Cleaned up useless codes

--- imports/ui/components/experience/ItemExternalDetails.jsx
+++ imports/ui/components/experience/ItemExternalDetails.jsx
@@ -1,47 +1,23 @@
 import React, { Component } from 'react';
 
 export default class ItemExternalDetails extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      windowWidth: window.innerWidth,
-      maxHeight: '500px',
-    };
-
-    this.handleResize = (event) => {
-      if (window.innerWidth >= 1350) {
-        this.setState({maxHeight: '200px'});
-      } else if (window.innerWidth >= 1000) {
-        this.setState({maxHeight: '300px'});
-      } else {
-        this.setState({maxHeight: '500px'});
-      }
-      this.setState({windowWidth: window.innerWidth});
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
 
   render() {
     return (
       <div className="hoverable" style={{borderRadius: '20px'}}>
         <div className="z-depth-2 experienceExpandable">
-          <div style={{cursor: 'pointer'}} onClick={this.toggleExpand}>
+          <a style={{cursor: 'pointer'}} href={this.props.link} target="_blank">
             <img src={this.props.imgSrc}
                  width={this.props.imgWidth}
                  height={this.props.imgHeight}
                  style={{borderRadius: '10px'}}/>
-            <div className="center hide-on-large-only" style={{marginTop: this.props.marginBelowPic + 'px'}}>
-              <a style={{color: '#455a64', fontFamily: 'Patua One, cursive'}}>View Details</a>
+            <div className="center" style={{marginTop: this.props.marginBelowPic + 'px'}}>
+              <div style={{color: '#455a64', fontFamily: 'Patua One, cursive', fontSize: '20px'}}>
+                {this.props.title}
+                <br />
+                View Details @ Github</div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     );
@@ -53,6 +29,8 @@ ItemExternalDetails.propTypes = {
   imgHeight: React.PropTypes.number.isRequired,
   imgWidth: React.PropTypes.number.isRequired,
   marginBelowPic: React.PropTypes.number.isRequired,
+  title: React.PropTypes.string.isRequired,
+  link: React.PropTypes.string.isRequired,
 };
 
 ItemExternalDetails.defaultProps = {
