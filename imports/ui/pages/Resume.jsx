1a6acf4eb5c2272827bb19b8a1bb5d3cadffd4b8
JeremyChen <jeremychen0127@gmail.com>
2016-08-28 19:30:14 -0400

    Removed pdf resume

--- imports/ui/pages/Resume.jsx
+++ imports/ui/pages/Resume.jsx
@@ -15,25 +15,6 @@ export default class Resume extends Component {
   render() {
     return (
       <div>
-        <div className="center">
-          <a href="files/Jeremy-Chen.pdf" download style={{marginRight: '50px'}}>
-            <RaisedButton
-              label="Download PDF"
-              backgroundColor="#7294a2"
-              labelColor="white"
-              labelPosition="before"
-              icon={<FontIcon className="material-icons">file_download</FontIcon>} />
-          </a>
-          <a href="files/Jeremy-Chen.pdf" target="_blank">
-            <RaisedButton
-              label="View PDF"
-              backgroundColor="#7294a2"
-              labelColor="white"
-              labelPosition="before"
-              icon={<FontIcon className="material-icons">open_in_new</FontIcon>} />
-          </a>
-        </div>
-        <br />
         <MyResume />
       </div>
     );
