04d2a253b370dc8a8d9d22ec7bcdba8a31667acf
JeremyChen <jeremychen0127@gmail.com>
2016-08-30 23:46:58 -0400

    Different designs for screens sizes

--- imports/ui/pages/Projects.jsx
+++ imports/ui/pages/Projects.jsx
@@ -16,6 +16,114 @@ export default class Projects extends Component {
     return {muiTheme: getMuiTheme(baseTheme)};
   }
 
+  renderTwoColumns() {
+    return (
+      <div className="row">
+        <div className="col l6">
+          <div style={styles.paperSpacingBottom}>
+            <div className="center">
+              <ItemExternalDetails
+                imgSrc="/img/json2xml2csv.png"
+                title="JSON/XML/CSV Converter"
+                link="https://github.com/jeremychen0127/Json2Xml2Csv"/>
+            </div>
+          </div>
+          <div style={styles.paperSpacingBottom}>
+            <div className="center">
+              <ItemExternalDetails
+                imgSrc="/img/sim4chat.png"
+                title="Simple 4 Chat Messaging App"
+                link="https://github.com/jeremychen0127/Simple4Chat"/>
+            </div>
+          </div>
+          <div style={styles.paperSpacingBottom}>
+            <div className="center">
+              <ItemExternalDetails
+                imgSrc="/img/tote.png"
+                imgHeight={200}
+                marginBelowPic={20}
+                title="Tote - Personal Stylist"
+                link="https://github.com/jeremychen0127/tote"/>
+            </div>
+          </div>
+        </div>
+        <div className="col l6">
+          <div style={styles.paperSpacingBottom}>
+            <div className="center">
+              <ItemExternalDetails
+                imgSrc="/img/pokemon-go-tsp.png"
+                title="Pokemon Go TSP (Contributor)"
+                link="https://github.com/jeremychen0127/PokemonGo-TSP"/>
+            </div>
+          </div>
+          <div style={styles.paperSpacingBottom}>
+            <div className="center">
+              <ItemExternalDetails
+                imgSrc="/img/tic-tac-toe.png"
+                marginBelowPic={20}
+                title="Tic-Tac-Toe with Unbeatable AI Player"
+                link="https://github.com/jeremychen0127/Tic_Tac_Toe"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderOneColumn() {
+    return (
+      <div>
+        <div className="row">
+          <div className="center">
+            <ItemExternalDetails
+              imgSrc="/img/json2xml2csv.png"
+              title="JSON/XML/CSV Converter"
+              link="https://github.com/jeremychen0127/Json2Xml2Csv"/>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="center">
+            <ItemExternalDetails
+              imgSrc="/img/pokemon-go-tsp.png"
+              title="Pokemon Go TSP (Contributor)"
+              link="https://github.com/jeremychen0127/PokemonGo-TSP"/>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="center">
+            <ItemExternalDetails
+              imgSrc="/img/sim4chat.png"
+              title="Simple 4 Chat Messaging App"
+              link="https://github.com/jeremychen0127/Simple4Chat"/>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="center">
+            <ItemExternalDetails
+              imgSrc="/img/tic-tac-toe.png"
+              marginBelowPic={20}
+              title="Tic-Tac-Toe with Unbeatable AI Player"
+              link="https://github.com/jeremychen0127/Tic_Tac_Toe"/>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="center">
+            <ItemExternalDetails
+              imgSrc="/img/tote.png"
+              imgHeight={200}
+              marginBelowPic={20}
+              title="Tote - Personal Stylist"
+              link="https://github.com/jeremychen0127/tote"/>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className="experience center fadeIn">
@@ -24,55 +132,12 @@ export default class Projects extends Component {
           <p className="quote center"><i>"The purpose of life is to contribute in some way to making things better" - Robert F. Kennedy</i></p>
         </div>
         <br />
-        <div className="row">
-          <div className={"col s12 " + ($(window).innerWidth() >= 1400 ? "l6" : "")}>
-            <div style={styles.paperSpacingBottom}>
-              <div className="center">
-                <ItemExternalDetails
-                  imgSrc="/img/json2xml2csv.png"
-                  title="JSON/XML/CSV Converter"
-                  link="https://github.com/jeremychen0127/Json2Xml2Csv"/>
-              </div>
-            </div>
-            <div style={styles.paperSpacingBottom}>
-              <div className="center">
-                <ItemExternalDetails
-                  imgSrc="/img/sim4chat.png"
-                  title="Simple 4 Chat Messaging App"
-                  link="https://github.com/jeremychen0127/Simple4Chat"/>
-              </div>
-            </div>
-            <div style={styles.paperSpacingBottom}>
-              <div className="center">
-                <ItemExternalDetails
-                  imgSrc="/img/tote.png"
-                  imgHeight={200}
-                  marginBelowPic={20}
-                  title="Tote - Personal Stylist"
-                  link="https://github.com/jeremychen0127/tote"/>
-              </div>
-            </div>
-          </div>
-          <div className={"col s12 " + ($(window).innerWidth() >= 1400 ? "l6" : "")}>
-            <div style={styles.paperSpacingBottom}>
-              <div className="center">
-                <ItemExternalDetails
-                  imgSrc="/img/pokemon-go-tsp.png"
-                  title="Pokemon Go TSP (Contributor)"
-                  link="https://github.com/jeremychen0127/PokemonGo-TSP"/>
-              </div>
-            </div>
-            <div style={styles.paperSpacingBottom}>
-              <div className="center">
-                <ItemExternalDetails
-                  imgSrc="/img/tic-tac-toe.png"
-                  marginBelowPic={20}
-                  title="Tic-Tac-Toe with Unbeatable AI Player"
-                  link="https://github.com/jeremychen0127/Tic_Tac_Toe"/>
-              </div>
-            </div>
-          </div>
-        </div>
+        {
+          $(window).innerWidth() >= 1400 ?
+            this.renderTwoColumns()
+          :
+            this.renderOneColumn()
+        }
         <br />
       </div>
     );
